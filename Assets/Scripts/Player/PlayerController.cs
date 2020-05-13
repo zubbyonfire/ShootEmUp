b9152c74ea9cabf62935f65973cab2ce73a7ee4e
Zack <zack@random42.com>
2020-05-13 23:44:29 +0100

    Added Player Movement
    To Add:
    - Player rotation to face mouse

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Handle core player logic: movement & shooting
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+    #region Movement
+    [Header("Movement Values")]
+    [SerializeField] private float movementForce;
+
+    private float movementLimiter = 0.7f;
+    private Vector3 movementInput = Vector3.zero;
+
+    private Rigidbody playerRigidbody = null;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get components
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementInput = new Vector3(Input.GetAxisRaw("Horizontal"),0 ,Input.GetAxisRaw("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        //Check if diagonal movement
+        if (movementInput.x != 0 && movementInput.z != 0)
+        {
+            movementInput *= movementLimiter;
+        }
+
+        //playerRigidbody.velocity = new Vector3(movementInput.x * movementForce, 0, movementInput.z * movementForce);
+        playerRigidbody.velocity = movementInput * movementForce;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMeshController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Handle logic for the player mesh - rotation
+/// </summary>
+public class PlayerMeshController : MonoBehaviour
+{
+    private Transform meshTransform = null;
+    [SerializeField] private Camera mainCamera = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get components
+        meshTransform = GetComponent<Transform>();
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MeshRotateToMouse();
+    }
+
+    /// <summary>
+    /// Rotate the mesh so it faces the mouse cursor
+    /// </summary>
+    private void MeshRotateToMouse()
+    {
+       
+    }
+}
